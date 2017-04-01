060d35abe0851d2fc60df1a03cfbe5222977381d
biwin <biwin007@gmail.com>
2017-04-01 15:09:12 +0530

    graphite as a blank module.

---
+++ django/db/backends/graphite/__init__.py
@@ -0,0 +1,5 @@
+# encoding=utf-8
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Biwin John Joseph <mail@biwinjohn.com> on 04 2016 for Project django
+# Copyright (C) Biwin John Joseph - All Rights Reserved
